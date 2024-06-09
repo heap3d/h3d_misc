bd78d1f00f04edb6e60ac5f103bdece0b4e64c39
Dmytro Holub <heap3d@gmail.com>
2024-06-09 18:05:02 +0300

    upd: print format upd: log filename

--- scripts/inspect_selected.py
+++ scripts/inspect_selected.py
@@ -14,13 +14,13 @@ from h3d_utilites.scripts.h3d_utils import replace_file_ext
 
 def inspect_graphs(items: list[modo.Item]):
     for item in items:
-        h3dd.print_debug(f'{item.name = }, {item.id = }, {item.type = }, type = {type(item)}')
+        h3dd.print_debug(f'{item.name=} {item.id=} {item.type=}')
         for graphname in item.itemGraphNames:
-            h3dd.print_debug(f'{graphname = }', 1)
+            h3dd.print_debug(f'{graphname=}', 1)
             for idx in item.itemGraph(graphname).connectedItems:
-                h3dd.print_debug(f'{idx = }', 2)
+                h3dd.print_debug(f'{idx=}', 2)
                 for item_detail in item.itemGraph(graphname).connectedItems[idx]:
-                    h3dd.print_debug(f'{item_detail.name = }, {item_detail.type = }, {item_detail = }', 3)
+                    h3dd.print_debug(f'{item_detail.name=}, {item_detail.type=}, {item_detail=}', 3)
 
 
 def main():
@@ -29,5 +29,5 @@ def main():
 
 
 if __name__ == '__main__':
-    h3dd = H3dDebug(enable=True, file=replace_file_ext(modo.Scene().filename, '.log'))
+    h3dd = H3dDebug(enable=True, file=replace_file_ext(modo.Scene().filename, '_inspect.log'))
     main()
